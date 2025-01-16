d2ed8944619ce57b5a023b68edcd7c0bcf01987a
2Shiro <animetales@naver.com>
2025-01-16 15:45:43 +0900

    carrot app v04

---
+++ flutter_carrot_app/lib/carrot_market_ui_06/screens/components/appbar_preferred_size.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+PreferredSize appBarBottomLine() {
+  return PreferredSize(
+    preferredSize: Size.fromHeight(0.5),
+    child: Divider(
+      thickness: 0.5,
+      height: 0.5,
+      color: Colors.grey,
+    ),
+  );
+}
---
+++ flutter_carrot_app/lib/carrot_market_ui_06/screens/my_carrot/components/card_icon_menu.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CardIconMenu extends StatelessWidget {
+  const CardIconMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
